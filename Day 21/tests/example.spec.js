676c90677a3e04ffe34b6580879ead01104b52b6
saikiran23-gh <pskiran1923@gmail.com>
2026-05-26 12:46:47 +0530

    updated jio mart test and authentication

--- Day 21/tests/example.spec.js
+++ Day 21/tests/example.spec.js
@@ -4,22 +4,22 @@ import { log } from 'node:console';
 
 test.describe('jio mart test', () => {
   test.use({ storageState: 'auth.json' });
-  test.skip('search for fruits', async ({ page }) => {
-    await page.goto('https://www.jiomart.com/sections/low-price-mumbai');
+  test('search for fruits', async ({ page }) => {
+    await page.goto('https://www.jiomart.com/sections/low-price-mumbai',{waitUntil:'commit'});
 
     // Click on the search input and type "fruits"
     const input = page.locator('//*[@id="app"]/div/div[3]/header/div[1]/div/div[2]/div[1]/div/div[2]/input');
-    await input.click();
+    await input.click({force:true});
     await input.fill('fruits');
     await input.press('Enter');
     await expect(page.getByText('fruits').first()).toBeVisible();
   });
 
-  test('profile', async ({ page }) => {
+  test.skip('profile', async ({ page }) => {
     await page.goto('https://www.jiomart.com/profile');
 
-    const profileName = page.getByTestId("JDSText-text").nth(0);
-    console.log(await profileName.textContent());
-    expect(profileName).toHaveText("Saikiran");
+    const profileName = page.getByText("Saikiran",{exact:true});
+    // console.log(await profileName.textContent());
+    await expect(profileName).toBeVisible();
   });
 });
